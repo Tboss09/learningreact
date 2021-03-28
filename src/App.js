92f383fdc0daae08d8a0b6b7ef34bb9ffef86771
Akosile Oluwatayo <69774441+Tboss09@users.noreply.github.com>
2021-03-28 03:31:38 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./style.scss";
+import "./styles.css";
+import Suscribe from "./components/Suscribe";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Suscribe />
+    </div>
+  );
+}
---
+++ src/components/Suscribe.js
@@ -0,0 +1,18 @@
+function Welcome(props) {
+  return (
+    <div className="Suscribe">
+      <h3>Suscribe</h3>
+      <p>Sign up with your email address</p>
+
+      <form className="signupForm">
+        <div className="inputField">
+          <input type="text" placeholder="Type name here" />
+          <input type="text" placeholder="Type Surname" />
+          <input type="email" placeholder="Type email here" />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+export default Welcome;
