4b813abe269265532d25ea7671478deeae24d26c
Akosile Oluwatayo <69774441+Tboss09@users.noreply.github.com>
2021-03-30 00:05:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import React from "react";
 import "./styles.css";
 import Suscribe from "./components/Suscribe";
 
-export default class App extends React.Component {
-
-  render() {  
-  const data = {
-    firstname:"Tayo AKosile",
-    button:"Click me"
+class TechList extends React.Component {
+  render() {
+    const { techs } = this.props;
+    return techs.map((tech, index) => <li > key ={index} {tech}</li>);
   }
-  
-    return(
-      <Suscribe data={data} title="I Just learnt react ckass methods"/>
-    )
+}
+
+export default class App extends React.Component {
+  render() {
+    const data = {
+      firstname: "Tayo AKosile",
+      button: "Click me"
+    };
+    const techs = ["HTML", "CSS", "JAVASRIPT", "REACT", "ANGULAR"];
+    return (
+      <div>
+        <Suscribe data={data} title="I Just learnt react class methods" />
+        <ul>
+          <TechList techs={techs} />
+        </ul>
+      </div>
+    );
   }
 }
